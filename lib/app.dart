129d9abd35a82abb71e24a1828c7af2907ca890f
Matheus Romão <matheus_barcellos@hotmail.com>
2019-12-04 01:19:36 -0300

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class InteractApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:interact_slides/app.dart';
+
+void main() => runApp(InteractApp());
+
